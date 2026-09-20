fc080ff2589ebcfe2d44fb3d00ad2d8a8f2550b5
tackler1112 <tackler101112@gmail.com>
2026-09-21 04:43:47 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}",
+    "./app/**/*.{js,jsx,ts,tsx,html}",
+    "./components/**/*.{js,jsx,ts,tsx,html}",
+    "./pages/**/*.{js,jsx,ts,tsx,html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
