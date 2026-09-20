264a979f5c76f2b05db45750cffb63c7049543af
tackler1112 <tackler101112@gmail.com>
2026-09-21 04:54:31 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx,html}",
-    "./app/**/*.{js,jsx,ts,tsx,html}",
-    "./components/**/*.{js,jsx,ts,tsx,html}",
-    "./pages/**/*.{js,jsx,ts,tsx,html}"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
