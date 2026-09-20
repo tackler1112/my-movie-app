e1d34d979eb2f714134981a9591ab753383c0c26
tackler1112 <tackler101112@gmail.com>
2026-09-21 07:17:23 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Hero } from './components/Hero';
-import { MovieCard } from './components/MovieCard';
-import { getGenres, getTrendingMovies, getNowPlayingMovies, discoverMovies } from './lib/tmdb';
-import { Movie, Genre } from './types';
+import { Hero } from './components/Hero.tsx';
+import { MovieCard } from './components/MovieCard.tsx';
+import { getGenres, getTrendingMovies, getNowPlayingMovies, discoverMovies } from './lib/tmdb.tsx';
+import { Movie, Genre } from './types.ts';
 
 function App() {
   const [genres, setGenres] = useState<Genre[]>([]);
